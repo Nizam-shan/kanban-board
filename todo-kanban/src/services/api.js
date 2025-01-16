a58314729356b9648b58ad909199353017941840
nizam-shan <nizamshan@xpayback.asia>
2025-01-16 10:04:14 +0530

    chages

--- todo-kanban/src/services/api.js
+++ todo-kanban/src/services/api.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000" });
+const api = axios.create({
+  baseURL: "https://kanban-board-bqwz.onrender.com/",
+});
 
 export const getTask = () => api.get("/all_task");
 export const createTask = (data) => api.post("/add_task", data);
 
 export const deleteTaskById = (id) => api.delete(`/delete_task/${id}`);
 export const updateTaskStatus = (taskId, status) =>
-  axios.put(`http://localhost:5000/update_task_status/${taskId}`, { status });
+  axios.put(
+    `https://kanban-board-bqwz.onrender.com/update_task_status/${taskId}`,
+    { status }
+  );
